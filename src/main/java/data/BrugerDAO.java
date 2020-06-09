9663e117a9edcb6c9ccaebf66fda1a2293cc3978
peterrevsbech <peter.revsbech@gmail.com>
2020-06-09 10:11:37 +0200

    Har autoudfyldt BrugerDAO

--- src/main/java/data/BrugerDAO.java
+++ src/main/java/data/BrugerDAO.java
@@ -1,4 +1,28 @@
 package data;
 
-public class BrugerDAO {
+import dto.BrugerDTO;
+
+import java.util.List;
+
+public class BrugerDAO implements iBrugerDAO{
+
+    @Override
+    public BrugerDTO getBruger(int oprId) throws DALException {
+        return null;
+    }
+
+    @Override
+    public List<BrugerDTO> getBrugerList() throws DALException {
+        return null;
+    }
+
+    @Override
+    public void createBruger(BrugerDTO opr) throws DALException {
+
+    }
+
+    @Override
+    public void updateBruger(BrugerDTO opr) throws DALException {
+
+    }
 }
