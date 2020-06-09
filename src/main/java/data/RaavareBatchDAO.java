ff2b8c2272a88e125b02aee00537f2643cdc59e3
peterrevsbech <peter.revsbech@gmail.com>
2020-06-09 16:04:59 +0200

    Er ved at lave RaavareBatchService, men den virker ikke helt

--- src/main/java/data/RaavareBatchDAO.java
+++ src/main/java/data/RaavareBatchDAO.java
@@ -1,4 +1,55 @@
 package data;
 
-public class RaavareBatchDAO {
+import dto.RaavareBatchDTO;
+import dto.RaavareDTO;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+public class RaavareBatchDAO implements iRaavareBatchDAO{
+    private RaavareDAO raavareDAO = new RaavareDAO();
+
+
+    @Override
+    public RaavareBatchDTO getRaavareBatch(int rbId) throws DALException {
+        DBconnector dBconnector = new DBconnector();
+        RaavareBatchDTO raavareBatchDTO = new RaavareBatchDTO();
+
+        try{
+            Statement statement = dBconnector.connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Raavarer WHERE rbId = "+rbId+";");
+            resultSet.next();
+            raavareBatchDTO.setRbId(resultSet.getInt(1));
+            raavareBatchDTO.setMaengde(resultSet.getDouble(3));
+            int raavareId = resultSet.getInt(2);
+            raavareBatchDTO.setRaavare(raavareDAO.getRaavare(raavareId));
+
+        }catch (Exception e){
+            throw new DALException("kunne ikke finde råvareBatch");
+        }
+
+        dBconnector.closeConnection();
+        return raavareBatchDTO;
+    }
+
+    @Override
+    public List<RaavareBatchDTO> getRaavareBatchList() throws DALException {
+        return null;
+    }
+
+    @Override
+    public List<RaavareBatchDTO> getRaavareBatchList(int raavareId) throws DALException {
+        return null;
+    }
+
+    @Override
+    public void createRaavareBatch(RaavareBatchDTO raavarebatch) throws DALException {
+
+    }
+
+    @Override
+    public void updateRaavareBatch(RaavareBatchDTO raavarebatch) throws DALException {
+
+    }
 }
--- src/main/java/rest/RaavareBatchService.java
+++ src/main/java/rest/RaavareBatchService.java
@@ -1,4 +1,23 @@
 package rest;
 
+import data.*;
+import dto.RaavareBatchDTO;
+import dto.RaavareDTO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("raavarebatch")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
 public class RaavareBatchService {
+    iRaavareBatchDAO raavareBatchDAO = new RaavareBatchDAO();
+
+    @GET //TODO Det her virker ikke pt
+    @Path("{id}")
+    public RaavareBatchDTO getUser(@PathParam("id") int id) throws DALException {
+        return raavareBatchDAO.getRaavareBatch(id);
+    }
+
 }
