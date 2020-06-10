322257307f420da974ae74dfb16902a3db47ff2a
peterrevsbech <peter.revsbech@gmail.com>
2020-06-10 15:02:37 +0200

    Har lavet ReceptDTO, ReceptDAO og ReceptService med GET

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,14 @@
-import data.BrugerDAO;
-import data.DALException;
-import data.ProduktBatchKompDAO;
-import data.RaavareBatchDAO;
+import data.*;
 import dto.BrugerDTO;
 import dto.RaavareBatchDTO;
+import dto.ReceptDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
     public static void main(String[] args) throws DALException {
-        ProduktBatchKompDAO produktBatchKompDAO = new ProduktBatchKompDAO();
-        produktBatchKompDAO.getProduktBatchKomp(51268945,75386425);
-
 
     }
 }
