1fc94a4c0d8ad8199ee3e7631076675c550403d2
peterrevsbech <peter.revsbech@gmail.com>
2020-06-11 14:54:47 +0200

    Er ved at tilføje atributten aktiv til BrugerDTO

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,7 +7,14 @@ import java.util.List;
 public class main {
 
     public static void main(String[] args) throws DALException {
+        BrugerDTO brugerDTO = new BrugerDTO(505,"Laborant","Tim","TT","1111111111",1);
+        BrugerDAO brugerDAO = new BrugerDAO();
+        brugerDAO.getBrugerList();
 
+
+        brugerDAO.createBruger(brugerDTO);
+        brugerDTO.setAktiv(0);
+        brugerDAO.updateBruger(brugerDTO);
     }
 }
 
