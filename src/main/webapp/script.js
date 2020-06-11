1672a3028ab0da2c1e5bee6c53e184ae3bda7047
CKyed <christiankyed@hotmail.com>
2020-06-11 15:20:14 +0200

    Er i gang med at udarbejde en login menu

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,8 +1,34 @@
-
-
-
 function switchPage(page){
     $("#container").load(page);
-
 }
+
+function checkLogin() {
+    var id =;
+    console.log(id);
+    event.preventDefault();
+    $.ajax({
+        url:'rest/bruger/'+id,
+        method: 'GET',
+        success: function (data) {
+            console.log(data.rolle);
+            switch (data.rolle) {
+                case "Administrator":
+                    switchPage('brugeradministrator.html');
+                    break;
+                case "Farmaceut":
+                    switchPage('farmcaeu.html');
+                    break;
+                case "Produktionsleder":
+                    switchPage('produktionsledelse.html');
+                    break;
+                case "Laborant":
+                    switchPage('laborant.html');
+                    break;
+            }
+        }
+    })
+}
+
+
+
 
