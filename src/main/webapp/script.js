ce8fe96795ec12bfc3b637465be2fa4ed2551920
Idkirsch <s195483@student.dtu.dk>
2020-06-11 16:04:45 +0200

    Rettet Js click me knap

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -6,12 +6,4 @@ function switchPage(page){
 
 }
 
-function showForm() {
-    var x = document.getElementById("createBruger");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
 
