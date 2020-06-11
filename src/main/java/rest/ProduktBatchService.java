8f02e58d718d5fff7deb3ae794a8228c963bdd13
peterrevsbech <peter.revsbech@gmail.com>
2020-06-11 11:27:15 +0200

    Tilføjet POST og PUT til ProduktBatchService

--- src/main/java/rest/ProduktBatchService.java
+++ src/main/java/rest/ProduktBatchService.java
@@ -3,10 +3,12 @@ package rest;
 import data.DALException;
 import data.ProduktBatchDAO;
 import data.iProduktBatchDAO;
+import dto.BrugerDTO;
 import dto.ProduktBatchDTO;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 
 @Path("produktbatch")
@@ -20,6 +22,22 @@ public class ProduktBatchService {
     @Path("{id}")
     public ProduktBatchDTO getProduktbatch(@PathParam("id") int id) throws DALException{
         return produktBatchDAO.getProduktBatch(id);
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createProduktBatchJson(ProduktBatchDTO produktBatchDTO) throws DALException {
+        produktBatchDAO.createProduktBatch(produktBatchDTO);
+        return Response.ok("Tilføjet").build();
+    }
+
+    @PUT
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateProduktBatchJson(ProduktBatchDTO produktBatchDTO) throws DALException {
+        produktBatchDAO.updateProduktBatch(produktBatchDTO);
+        return Response.ok("Opdateret").build();
     }
 
 
