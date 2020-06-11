e3dd1fcbcf18dd9f672af6da09b7f63856842827
peterrevsbech <peter.revsbech@gmail.com>
2020-06-11 10:48:07 +0200

    Færdiggjort ProduktBatchDAO

--- src/main/java/data/ProduktBatchDAO.java
+++ src/main/java/data/ProduktBatchDAO.java
@@ -2,9 +2,11 @@ package data;
 
 
 import dto.ProduktBatchDTO;
+import dto.ProduktBatchKompDTO;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProduktBatchDAO implements iProduktBatchDAO {
@@ -36,16 +38,87 @@ public class ProduktBatchDAO implements iProduktBatchDAO {
 
     @Override
     public List<ProduktBatchDTO> getProduktBatchList() throws DALException {
-        return null;
+        DBconnector dBconnector = new DBconnector();
+        ProduktBatchDTO produktBatchDTO = new ProduktBatchDTO();
+        List<ProduktBatchDTO> produktBatchDTOList = new ArrayList<ProduktBatchDTO>();
+
+        try{
+            Statement statement = dBconnector.connection.createStatement();
+            String SQLquery = "SELECT * FROM ProduktBatches;";
+            ResultSet resultSet = statement.executeQuery(SQLquery);
+            while (resultSet.next()){
+                produktBatchDTO.setPbId(resultSet.getInt(1));
+                produktBatchDTO.setStatus(resultSet.getInt(2));
+                produktBatchDTO.setReceptId(resultSet.getInt(3));
+                produktBatchDTO.setProduktBatchKomponenter(produktBatchKompDAO.getProduktBatchKompList(resultSet.getInt(1)));
+                produktBatchDTOList.add(produktBatchDTO);
+                produktBatchDTO = new ProduktBatchDTO();
+            }
+
+
+        }catch (Exception e){
+            throw new DALException("Kunne ikke finde listen af produktbatches");
+        }
+        dBconnector.closeConnection();
+        return produktBatchDTOList;
     }
 
     @Override
     public void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException {
+        DBconnector dBconnector = new DBconnector();
 
+        try {
+            Statement statement = dBconnector.connection.createStatement();
+            //Create String for the SQL Insert Statement
+            String SQLstatement = "insert into ProduktBatches values('%d', '%d', '%d');";
+            //Format the string
+            SQLstatement =String.format(SQLstatement,
+                    produktbatch.getPbId(),
+                    produktbatch.getStatus(),
+                    produktbatch.getReceptId()); //Assume that the recept has already been created
+
+            //Execute the insert statement
+            statement.executeUpdate(SQLstatement);
+
+            //Insert the components of the recept in the database
+            //Add each element from the list to the database
+            for (ProduktBatchKompDTO pbkDTO: produktbatch.getProduktBatchKomponenter()) {
+                produktBatchKompDAO.createProduktBatchKomp(pbkDTO);
+            }
+
+
+        }catch (Exception e){
+            throw new DALException("Kunne ikke oprette bruger med det ID");
+        }
+
+        dBconnector.closeConnection();
     }
 
     @Override
     public void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException {
+        DBconnector dBconnector = new DBconnector();
 
+        try {
+            Statement statement = dBconnector.connection.createStatement();
+            //Create String for the SQL Insert Statement
+            String SQLstatement = "UPDATE ProduktBatches SET receptId = %d , statuss = %d;";
+            //Format the string
+            SQLstatement =String.format(SQLstatement,
+                    produktbatch.getReceptId(), //Assume that the recept has already been created
+                    produktbatch.getStatus());
+            //Execute the insert statement
+            statement.executeUpdate(SQLstatement);
+
+            //Update the components of the recept in the database
+            for (ProduktBatchKompDTO pbkDTO: produktbatch.getProduktBatchKomponenter()) {
+                produktBatchKompDAO.updateProduktBatchKomp(pbkDTO);
+            }
+
+
+        }catch (Exception e){
+            throw new DALException("Kunne ikke oprette bruger med det ID");
+        }
+
+        dBconnector.closeConnection();
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,5 @@
 import data.*;
-import dto.BrugerDTO;
-import dto.ProduktBatchDTO;
-import dto.RaavareBatchDTO;
-import dto.ReceptDTO;
+import dto.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,9 +7,25 @@ import java.util.List;
 public class main {
 
     public static void main(String[] args) throws DALException {
-        ProduktBatchDAO pbd = new ProduktBatchDAO();
-        ProduktBatchDTO pro = pbd.getProduktBatch(51268945);
+        /*
+        RaavareDTO raavareDTO = new RaavareDTO(10102121,"Tis","Tissemand");
+        RaavareBatchDTO raavareBatchDTO = new RaavareBatchDTO(11113333,raavareDTO,2.0);
+        RaavareBatchDTO raavareBatchDTO2 = new RaavareBatchDTO(11112222,raavareDTO,1.0);
+        BrugerDTO bruger = new BrugerDTO(98,"Laborant","Yao Ming","YM","12312312");
+        ProduktBatchKompDTO produktBatchKompDTO = new ProduktBatchKompDTO(raavareBatchDTO,4.4,2.2,bruger);
+        ProduktBatchKompDTO produktBatchKompDTO2 = new ProduktBatchKompDTO(raavareBatchDTO2,4.0,2.0,bruger);
+        ArrayList<ProduktBatchKompDTO> komponenter = new ArrayList<ProduktBatchKompDTO>();
+        komponenter.add(produktBatchKompDTO);
+        komponenter.add(produktBatchKompDTO2);
+        ProduktBatchDTO produktBatchDTO = new ProduktBatchDTO(44433322,23695842,0,komponenter);
 
+
+        ProduktBatchDAO pbd = new ProduktBatchDAO();
+        pbd.createProduktBatch(produktBatchDTO);
+
+         */
+        ProduktBatchDAO pbd = new ProduktBatchDAO();
+        List<ProduktBatchDTO> pbDTO = pbd.getProduktBatchList();
 
 
     }
