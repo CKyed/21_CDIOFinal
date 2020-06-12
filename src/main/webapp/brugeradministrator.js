48023fd961842c44674b569a65b2f2bfbec3c08d
Idkirsch <s195483@student.dtu.dk>
2020-06-12 11:56:52 +0200

    Kan finde en bruger på ID og logge brugeren til konsollen

--- src/main/webapp/brugeradministrator.js
+++ src/main/webapp/brugeradministrator.js
@@ -17,8 +17,8 @@ function generateBrugerTable(bruger){
         '<td>' + bruger.rolle + '</td>' +
         '<td>' + bruger.brugerNavn + '</td>' +
         '<td>' + bruger.initialer + '</td>' +
-        '<td>' + bruger.aktiv + '</td>' +
-        '<td>' + bruger.cpr + '</td>'
+        '<td>' + bruger.cpr + '</td>' +
+        '<td>' + bruger.aktiv + '</td>'
 }
 
 function createBruger() {
@@ -41,18 +41,71 @@ function createBruger() {
         }
     })
 
+}
+
+function loadSpecificBruger() {
+    var id = document.getElementById("BrugerId").value;
+    console.log(id);
+    $.ajax({
+        method: 'GET',
+        url:'rest/bruger/'+id,
+        success: function (data) {
+            console.log(data);
+        }
+    })
 
 }
 
-function showForm() {
+
+
+
+
+function showCreateBruger() {
     var x = document.getElementById("createBruger");
     if (x.style.display === "block") {
         x.style.display = "none";
 
     } else {
         x.style.display = "block";
-
     }
 }
 
+function showUpdateBruger() {
+    var x = document.getElementById("updateBruger");
+    if (x.style.display === "block") {
+        x.style.display = "none";
 
+    } else {
+        x.style.display = "block";
+    }
+
+
+/*function checkLogin() {
+    var id =document.getElementById("loginBrugerID").value;
+    console.log(id);
+    $.ajax({
+        method: 'GET',
+        url:'rest/bruger/'+id,
+        success: function (data) {
+            console.log(data);
+            switch (data.rolle) {
+                case "Administrator":
+                    switchPage('brugeradministrator.html');
+                    break;
+                case "Farmaceut":
+                    switchPage('farmaceut.html');
+                    break;
+                case "Produktionsleder":
+                    switchPage('produktionsleder.html');
+                    break;
+                case "Laborant":
+                    switchPage('laborant.html');
+                    break;
+            }
+        }
+  })
+}
+*/
+
+
+}
