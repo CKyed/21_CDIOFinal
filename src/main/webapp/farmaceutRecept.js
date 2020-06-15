eb333a5851223b5334cf2f6a189ff21078a77085
peterrevsbech <peter.revsbech@gmail.com>
2020-06-15 21:48:03 +0200

    Er ved at omstrukturere farmaceut-delen så der bliver to dokumenter i stedet for 1

--- src/main/webapp/farmaceutRecept.js
+++ src/main/webapp/farmaceutRecept.js
@@ -4,8 +4,6 @@ $(document).ready(function () {
 });
 var raavareList = [];
 var raavareOptionList = document.getElementById('raavare');
-var raavareTable = document.getElementById('raavaretable');
-
 var receptList = [];
 var maxReceptId = 99999999;
 var maxTolerance = 10;
@@ -56,112 +54,21 @@ function loadRaavarer() {
         $.each(data, function (i, elt) {
             raavareList.push(elt);
         });
-        generateRaavareTable();
         generateRaavareOptionList();
     });
-}
-function generateRaavareTable() {
-    //Make sure, raavareTable is updated
-    raavareTable = document.getElementById('raavaretable');
-
-    //fill the table with elements from raavareList
-    $.each(raavareList, function (i, elt) {
-        var row = raavareTable.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        cell1.innerHTML = elt.raavareID;
-        cell2.innerHTML = elt.raavareNavn;
-        cell3.innerHTML = elt.leverandoer;
-    });
-
-    //set the table in the HTML document by id
-    $('#raavaretable').value = raavareTable;
-}
-
-
-function createRaavare() {
-    event.preventDefault();
-    var data =$('#raavareForm').serializeJSON();
-    console.log(data);
-    $.ajax({
-        url: 'rest/raavare',
-        method: 'POST',
-        contentType: "application/json",
-        data: data,
-        success: function (data) {
-            alert(JSON.stringify(data));
-            loadRaavarer();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alert(jqXHR.responseText);
-            alert(textStatus);
-            alert(errorThrown);
-        }
-    })
-}
-
-function updateRaavare() {
-    event.preventDefault();
-    var data =$('#raavareUdateForm').serializeJSON();
-    console.log(data);
-    $.ajax({
-        url: 'rest/raavare',
-        method: 'PUT',
-        contentType: "application/json",
-        data: data,
-        success: function (data) {
-            alert(JSON.stringify(data));
-            loadRaavarer();
-        },
-        error: function (jqXHR) {
-            alert(jqXHR.responseText);
-        }
-
-    })
 }
 
 function generateRaavareOptionList() {
     raavareOptionList = document.getElementById('raavare');
 
-
-    var raavareOptionHTML;
-    var navn;
-    var id;
     $.each(raavareList, function (i, elt) {
         var raavareOption = document.createElement("OPTION");
-        raavareOption.setAttribute("value", elt.raavareNavn + "; " + elt.raavareID);
-        var t = document.createTextNode(elt.raavareNavn + "; " + elt.raavareNavn);
+        raavareOption.setAttribute("value", i);
+        var t = document.createTextNode(elt.raavareNavn + "; " + elt.raavareID);
         raavareOption.appendChild(t);
-        raavareOptionList.appendChild(x);
-
-        //var navn =elt.raavareNavn;
-        //var id = elt.raavareID;
-        //raavareOptionHTML = '<option> ' +navn+ '; ' + id + ' </option>';
-        //$('#raavare').append(raavareOptionHTML);
+        raavareOptionList.appendChild(raavareOption);
     });
-    document.getElementById("raavare").append(raavareOptionList);
-}
 
-function loadSpecificRaavare() {
-    var id = document.getElementById("RaavareId").value;
-    var errormessage;
-    errormessage = document.getElementById("errorMessage");
-    //  console.log(id);
-    $.ajax({
-        method: 'GET',
-        url:'rest/raavare/'+id,
-        success: function (data) {
-            errormessage.innerHTML="";
-            console.log(data);
-            console.log(data.aktiv);
-            document.getElementById("updateRaavareNavn").value = data.raavareNavn;
-            document.getElementById("updateLeverandoer").value = data.leverandoer;
-        },
-        error: function () {
-            errormessage.innerHTML="Kunne ikke finder råvarer med det ID, prøv igen";
-        }
-    })
 }
 
 function addReceptKomp() {
@@ -171,38 +78,27 @@ function addReceptKomp() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    cell1.innerHTML = document.getElementById("raavare").value;
-    cell2.innerHTML = document.getElementById("netto").value;
-    cell3.innerHTML = document.getElementById("tolerance").value;
-    cell4.innerHTML = '<td><input type="button" value="Slet linje" onclick="deleteRow()"></td>'
+    var cell5 = row.insertCell(4);
+    var selectedRaavareId = raavareList[document.getElementById("raavare").value].raavareID;
+    cell1.innerHTML = raavareList[document.getElementById("raavare").value].raavareNavn;
+    cell2.innerHTML = selectedRaavareId;
+    cell3.innerHTML = document.getElementById("netto").value;
+    cell4.innerHTML = document.getElementById("tolerance").value;
+    cell5.innerHTML = '<td><input type="button" value="Slet linje" onclick="deleteRow()"></td>';
+
+
+    $.each(raavareList, function (i, elt) {
+        if (elt.raavareID ===selectedRaavareId){
+            raavareList.remove(i);
+        }
+    });
+
 }
 
 function deleteRow() {
     var td = event.target.parentNode;
     var tr = td.parentNode;
     tr.parentNode.removeChild(tr);
-}
-
-function lockReceptForm() {
-    var receptID =document.getElementById('receptid').value;
-    var valid = receptIdVacant(receptID);
-
-    if (receptID> maxReceptId){
-        alert("Receptens ID er for langt.");
-    } else if (receptList.length ==0){
-        alert("Vent et øjeblik til recepterne er loadet færdigt.");
-    } else if (valid){
-        document.getElementById('receptid').disabled = true;
-        document.getElementById('receptnavn').disabled = true;
-        document.getElementById('lockReceptIdButton').disabled = true;
-        document.getElementById('raavare').disabled = false;
-        document.getElementById('netto').disabled = false;
-        document.getElementById('tolerance').disabled = false;
-    } else if (!valid){
-        alert("Receptens ID er allerede i brug.");
-    } else {
-        alert("Uventet fejl.")
-    }
 }
 
 function checkIfReceptIdValid() {
@@ -238,8 +134,6 @@ function saveReceptToDatabase() {
         alert("Noget gik galt");
         return;
     }
-
-
 
     event.preventDefault();
 
@@ -284,13 +178,12 @@ function getReceptKomponenterJSON() {
             "nonNetto" : $(tr).find('td:eq(1)').text()
             , "tolerance" : $(tr).find('td:eq(2)').text()
             , "raavare" : {
-            "raavareID": raavareId,
+                "raavareID": raavareId,
                 "raavareNavn": "ligemeget",
                 "leverandoer": "ligemeget"
-        } ,"receptId" : document.getElementById('receptid').value
+            } ,"receptId" : document.getElementById('receptid').value
         }
     });
-    //TableData.shift();  // first row will be empty - so remove
     console.log(TableData);
     return TableData;
 }
