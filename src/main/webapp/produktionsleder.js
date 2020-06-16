6d9844de2369fd83e15f52ce086d2dc53b7e737c
haida <Haidariano@hotmail.com>
2020-06-16 08:30:55 +0200

    små rettelser i html

--- src/main/webapp/produktionsleder.js
+++ src/main/webapp/produktionsleder.js
@@ -42,42 +42,4 @@ function createRaavareBatch() {
     })
 }
 
-/*
-function addReceptKomp1() {
-    var HTML = '<tr><td>' + 'receptId' + '</td>' +
-        '<td>' + document.getElementById("raavare").value + '</td>' +
-        '<td>' + document.getElementById("netto").value + '</td>' +
-        '<td>' + document.getElementById("tolerance").value + '</td>' +
-        '<td><button>Slet linje</button></td></tr> '
-    console.log(HTML);
-    $('#receptkomptablebody').append(HTML);
 
-}
-
-function  generateRaavareList(raavare) {
-    return '<option>' +raavare.raavareNavn +'</option>'
-}
-
-function addReceptKomp() {
-    var table = document.getElementById("receptkomptablebody");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    cell1.innerHTML = document.getElementById("receptid").value;;
-    cell2.innerHTML = document.getElementById("raavare").value;
-    cell3.innerHTML = document.getElementById("netto").value;
-    cell4.innerHTML = document.getElementById("tolerance").value;
-    cell5.innerHTML = '<td><input type="button" value="Slet linje" onclick="deleteRow()"></td>'
-
-}
-
-function deleteRow() {
-    var td = event.target.parentNode;
-    var tr = td.parentNode;
-    tr.parentNode.removeChild(tr);
-}
-
-*/
